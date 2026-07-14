7f519df54dd4894b25b1f0457fb2700fafb47085
Hosting-web <pablospim581@gmail.com>
2026-07-15 01:32:55 +0200

    Delete api directory

--- api/click.js
+++
@@ -1,9 +0,0 @@
-import { kv } from '@vercel/kv';
-
-export default async function handler(req, res) {
-  const { key } = req.query;
-  if (!key) return res.status(400).json({ error: 'Falta el parámetro key' });
-
-  const newCount = await kv.incr(`clicks:${key}`);
-  return res.status(200).json({ key, count: newCount });
-}
